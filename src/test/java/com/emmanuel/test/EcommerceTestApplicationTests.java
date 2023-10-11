4316a50303772b847012dd7d11cfa0d34e583854
emmanuel leal <emmanuel_leal_ortiz@gmail.com>
2023-10-11 16:31:49 -0600

    first change

---
+++ src/test/java/com/emmanuel/test/EcommerceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
